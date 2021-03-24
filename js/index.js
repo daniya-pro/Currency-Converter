916fc5d73a9ebc2ceadbc48d52cd4c995ba9d237
daniya-pro <syedadaniyahaider@gmail.com>
2021-03-25 01:15:17 +0500

    created Currency converter
    Use this website for converting any currency to any other currency like 1000 PKR to USD = check inside the website link is available

--- js/index.js
+++ js/index.js
@@ -1,22 +1,59 @@
-var valec1;
-var valecc2;
-function drdown(){
+//all variables start herevar
+var switchFrom = document.getElementById("switchFrom")
 
-    var e = document.getElementById("currencyList");
-    var e2 = document.getElementById("currencyList2");
-    var value = e.value;
-    var value2 = e2.value;
+ var setT=setTimeout(
+     switchFrom.addEventListener("keydown", fetchfunc)
+,300)
+var e = document.getElementById("currencyList");
+var e2 = document.getElementById("currencyList2");
+var switchedValue =document.getElementById("switchedValue");
+var va1 = e.value;
+var va2 = e2.value;
+//all variables ends here
+//functions starts
+function drdown(data) {
+  var value1 = e.value;
+  var value2 = e2.value;
 
-if(value === value2){
-
-
+  //var IdName =data.currentTarget.id;
+  console.log(data);
+  if (value1 === value2) {
+    switchCurrency();
+ 
+  }
+ va1 = e.value;
+  va2 = e2.value;
+ fetchfunc()
 
 }
+function fetchfunc() {
+    if(typeof setT !== "undefined"){
+        clearTimeout(setT);
+      } 
+    switchFrom.value.replace(/\D+/g, '')
+    fetch(
 
+    `https://free.currconv.com/api/v7/convert?q=${va1}_${va2}&compact=ultra&apiKey=ef6861ff62c446fce20a`
+  )
+    .then((d) => d.json())
+    .then((data) =>{ 
+      
+        console.log(data);
+    var switchTo =data[Object.keys(data)[0]];
+
+ switchedValue.value= (switchTo*switchFrom.value).toFixed(2);
+console.log(switchFrom.value,switchedValue.value)
 }
+    );}
+function switchCurrency() {
+  e2.value = va1;
+  e.value = va2;
+  va1 = e.value;
+  va2 = e2.value;}
+//functions ends here
 
-document.getElementById('currencyList').addEventListener('change', drdown)
+//addEventListener starts here
+e.addEventListener("change", drdown);
+e2.addEventListener("change", drdown);
 
-
-
-fetch("https://free.currconv.com/api/v7/convert?q=PKR_USD&compact=ultra&apiKey=ef6861ff62c446fce20a").then(d =>d.json()).then(data =>console.log(data))
+//addeventlistener ends here
